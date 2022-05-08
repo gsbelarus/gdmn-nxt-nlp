d6ba8f18351f67e93f99f86a32e83f74151b6aa6
andreik <gs1994@gmail.com>
2022-05-08 19:36:43 +0300

    loading model

--- src/server/server.py
+++ src/server/server.py
@@ -1,15 +1,27 @@
 import json
+from jinja2 import Undefined
 import spacy
 from spacy.language import Language
 from spacy_langdetect import LanguageDetector
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from transformers import pipeline
+import os
+from dotenv import load_dotenv
+import requests
 
-#spacy.prefer_gpu()
+load_dotenv(dotenv_path='../gdmn-nxt/.env')
+gdmn_nxt_server = 'http://localhost:{0}'.format(
+    os.getenv('GDMN_NXT_SERVER_PORT'))
+print('Detected address of gdmn-nxt server: {0}'.format(
+    gdmn_nxt_server))
+
+# spacy.prefer_gpu()
+
 
 @Language.factory('language_detector')
 def create_language_detector(nlp, name):
     return LanguageDetector(language_detection_function=None)
+
 
 en_model_name = 'en_core_web_trf'
 print('Loading model {0}...'.format(en_model_name))
@@ -30,6 +42,10 @@ serverPort = 8080
 
 intent_labels = ["show", "update", "insert", "delete"]
 
+erModel = {}
+erModel_fullDbName = ''
+
+
 class NLPServer(BaseHTTPRequestHandler):
     def do_OPTIONS(self):
         self.send_response(200, 'ok')
@@ -38,15 +54,17 @@ class NLPServer(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Headers', 'X-Requested-With')
         self.send_header('Access-Control-Allow-Headers', 'Content-Type')
         self.end_headers()
-            
+
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(bytes('<html><head><title>https://pythonbasics.org</title></head>', 'utf-8'))
+        self.wfile.write(
+            bytes('<html><head><title>https://pythonbasics.org</title></head>', 'utf-8'))
         self.wfile.write(bytes('<p>Request: %s</p>' % self.path, 'utf-8'))
         self.wfile.write(bytes('<body>', 'utf-8'))
-        self.wfile.write(bytes('<p>This is an example web server.</p>', 'utf-8'))
+        self.wfile.write(
+            bytes('<p>This is an example web server.</p>', 'utf-8'))
         self.wfile.write(bytes('</body></html>', 'utf-8'))
 
     def do_POST(self):
@@ -61,15 +79,32 @@ class NLPServer(BaseHTTPRequestHandler):
         print(post_body)
 
         if (self.path == '/api/nlp/v1/parse-text'):
-            if post_body['language'] == 'en': 
-              doc = en_nlp(post_body['text'])
-              model = en_model_name
-            elif post_body['language'] == 'ru':   
-              doc = ru_nlp(post_body['text'])
-              model = ru_model_name
+            if post_body['language'] == 'en':
+                doc = en_nlp(post_body['text'])
+                model = en_model_name
+            elif post_body['language'] == 'ru':
+                doc = ru_nlp(post_body['text'])
+                model = ru_model_name
             else:
-              print('Unsupported language: {0}'.format(post_body['language']))    
-              return
+                print('Unsupported language: {0}'.format(
+                    post_body['language']))
+                return
+
+            global erModel
+            global erModel_fullDbName
+
+            if erModel_fullDbName != post_body['fullDbName']:
+                url = gdmn_nxt_server + '/api/v1/er-model'
+                r = requests.get(url) 
+                if r.status_code == 200:
+                    erModel = r.json() 
+                    erModel_fullDbName = erModel['fullDbName']
+                    print('Loaded ER model: {0}'.format(erModel_fullDbName))
+                    print('Entities: {0}...'.format(len(erModel['entities'].keys())))
+                    print('Domains: {0}...'.format(len(erModel['domains'].keys())))
+                else:
+                    print('Failed to get ER model from {0}'.format(url))
+                    return
 
             sents = []
 
@@ -105,7 +140,7 @@ class NLPServer(BaseHTTPRequestHandler):
 
                     tokens.append(t)
 
-                ents = []    
+                ents = []
 
                 for ent in sent.ents:
                     e = {
@@ -118,7 +153,7 @@ class NLPServer(BaseHTTPRequestHandler):
                     ents.append(e)
 
                 res = classifier(sent.text, intent_labels)
-    
+
                 sents.append({
                     'detectedLanguage': sent._.language,
                     'text': sent.text,
@@ -140,9 +175,10 @@ class NLPServer(BaseHTTPRequestHandler):
             }
             self.wfile.write(bytes(json.dumps(data), 'utf-8'))
         else:
-            self.wfile.write(bytes(json.dumps({ 'answer': 'text' }), 'utf-8'))
+            self.wfile.write(bytes(json.dumps({'answer': 'text'}), 'utf-8'))
 
-if __name__ == '__main__':        
+
+if __name__ == '__main__':
     webServer = HTTPServer((hostName, serverPort), NLPServer)
     print('Server started http://%s:%s' % (hostName, serverPort))
 
