f807af132cf6f992fdaa5a33f75dfce4d9e67f9a
andreik <gs1994@gmail.com>
2022-07-28 12:27:51 +0200

    upgrade cuda to 11.6.2

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
-pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu117
-pip install -U "spacy[cuda117,transformers,lookups]"
+pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
+pip install -U "spacy[cuda116,transformers,lookups]"
 python -m spacy download en_core_web_trf
 python -m spacy download pl_core_news_lg
 python -m spacy download ru_core_news_lg
@@ -11,4 +11,4 @@ pip install -U spacy_langdetect
 pip install -U python-dotenv
 pip install -U spacy_universal_sentence_encoder
 pip install -U sentencepiece
-pip install -U matplotlib.pyplot
+pip install -U matplotlib
